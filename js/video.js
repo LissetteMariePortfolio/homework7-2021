f3b2b34bcc7f37d43729297a77989a0a55d1a126
LissetteMariePortfolio <89919777+LissetteMariePortfolio@users.noreply.github.com>
2021-11-18 20:26:20 -0500

    Fixed the JS file

--- js/video.js
+++ js/video.js
@@ -1,17 +1,18 @@
 
 
+
 var video = document.getElementById("player1");
 var muteButton = document.getElementById("mute");
 
 var slider = document.getElementById("slider");
 var body = document.querySelector('body');
-var getVol = 100;
+var getVol = "100%";
 var volume = document.getElementById("volume");
-volume.innerText = getVol;
+
  document.querySelector("#play").addEventListener("click", function() {
  	video.play();
 	console.log("Play Video");
-	volume.innerText = getVol;
+	volume.innerHTML = getVol;
  });
  document.querySelector("#pause").addEventListener("click", function() {
 	video.pause();
@@ -32,28 +33,33 @@ document.querySelector("#faster").addEventListener("click", function() {
 
  });
  document.querySelector("#skip").addEventListener("click", function() {
-
+    if (video.currentTime >= 52.403333){
+		video.currentTime = 0;
+	}
+	else{
 	video.currentTime += 15;
+	}
 	console.log("Video time is now:" + video.currentTime );
 
 
  });
  document.querySelector("#mute").addEventListener("click", function() {
-    getVol = video.volume;
+    volumeStorage = video.volume;
+	getVol = String(video.volume) + "%";
 	if (muteButton.innerHTML == "Mute"){
 	
 	console.log(getVol);
 	muteButton.innerHTML = "Unmute";
 	video.volume = 0;
-	volume.innerText = 0;
-	slider.value = 0;
+	volume.innerHTML = "0%";
+	slider.value = "0%";
 	
 	}
 	else{
 	muteButton.innerHTML = "Mute";
-	video.volume = getVol;
-	volume.innerText = 0;
-	slider.innerHTML = getVol;
+	video.volume = volumeStorage;
+	volume.innerHTML = getVol;
+	
 	}
 	console.log("Video volume is now:" + video.volume );
 
@@ -61,22 +67,22 @@ document.querySelector("#faster").addEventListener("click", function() {
 
   document.querySelector("#vintage").addEventListener("click", function() {
 
-	body.classList.add("oldSchool");
+	video.classList.add("oldSchool");
 	console.log("Attempted to apply Old School Class" );
 
 
  });
 slider.addEventListener('change',function(){
     video.volume = this.value / 100;
-	volume.innerHTML = this.value;
+	volume.innerHTML = String(this.value) + "%";
 });
  document.querySelector("#orig").addEventListener("click", function() {
 
-	body.classList.remove("oldSchool");
+	video.classList.remove("oldSchool");
 	console.log("Attempted to remove Old School Class");
 
 
  });
-
+  
 
 
